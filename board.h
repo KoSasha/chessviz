56ae8980c16cca70c553038b8cd4fbb16db17cce
KoSasha <sasha_pishi99@mail.ru>
2018-04-04 17:15:09 +0200

    Board.h

---
+++ board.h
@@ -0,0 +1,20 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    char array[8];
+    char type;
+}board;
+
+void initial_board(board *a);
+
+void print_board(board *a);
+
+void coordinates(board *a);
+
+void swap(board *a, char i, int j, char i1, int j1);
+
+#endif
