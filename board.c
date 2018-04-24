688e8bc620fc3d9b88bae86f0596759a7e27e5c1
KoSasha <sasha_pishi99@mail.ru>
2018-04-24 07:19:37 +0200

    Association2

--- board.c
+++ board.c
@@ -32,4 +32,9 @@ int coordinates(board *a, char i, int j, char i1, int j1) {
     return 0;
 }
 
-
+void swap(board *a, char i, int j, char i1, int j1){
+    int tmp;
+    tmp = a->array[j * 8 + i];
+    a->array[j * 8 + i] = a->array[j1 * 8 + i1];
+    a->array[j1 * 8 + i1] = tmp;
+}
